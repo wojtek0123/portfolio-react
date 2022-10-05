180c68837b9ddf2f85dcfa938b8f434b799f8262
wojtek <wojtekpietraszuk@gmail.com>
2022-10-05 19:37:58 +0200

    Add slide animation and buttons to nav
    Add custom animation because built in tailwind animation doesn't fit. Also change to buttons with changing screen. I think this will be more responsive for all mobile device

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import backgroundImage from './assets/undraw_startup_life.svg';
 
 const App = () => (
-	<div className='bg-black h-screen w-screen text-white relative overflow-hidden'>
-		<header className='flex flex-col justify-around items-start absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/3 w-full'>
+	<div className='flex flex-col justify-between bg-black h-screen w-screen text-white relative overflow-x-hidden'>
+		<header className='flex flex-col justify-center h-full items-start w-full'>
 			<div className='mb-10 px-5'>
 				<span className='text-5xl font-bold'>Hello!</span>
 				<h1 className='text-4xl font-bold mt-5'>I am a Junior Frontend Developer</h1>
 			</div>
-			<div className='flex items-center justify-between text-center w-full px-5 mb-5'>
-				<div className=''>slide left</div>
-				<div className=''>slide right</div>
+			<div className='flex items-center justify-between text-center w-full px-5 mb-10'>
+				<div className='animate-[slide-left_1s_infinite_alternate]'>slide left</div>
+				<div className='animate-[slide-right_1s_infinite_alternate]'>slide right</div>
 			</div>
 			<img src={backgroundImage} alt='' />
 		</header>
-		<nav className='flex justify-between px-5 items-center fixed bottom-5 left-0 right-0 text-center'>
-			<span className='text-xl uppercase'>about me</span>
+		<nav className='flex justify-between px-5 items-center bg-black bottom-0 left-0 right-0 text-center pb-5'>
+			<button className='text-xl bg-white text-black py-1 px-3 rounded-lg' type='button'>
+				prev
+			</button>
 			<span className='text-xl uppercase'>home</span>
-			<span className='text-xl uppercase'>work</span>
-			<span className='text-xl uppercase'>contact</span>
+			<button className='text-xl bg-white text-black py-1 px-3 rounded-lg' type='button'>
+				next
+			</button>
 		</nav>
 	</div>
 );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				'slide-left': {
+					'0%': {transform: 'translateX(0)'},
+					'100%': {transform: 'translateX(-10%)'},
+				},
+				'slide-right': {
+					'0%': {transform: 'translateX(0)'},
+					'100%': {transform: 'translateX(10%)'},
+				},
+			},
+		},
 		fontFamily: {
 			'oswold-font': ['Oswald', 'sans-serif'],
 		},
