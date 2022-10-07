d2b491b04063d77eefb134c5a700dd5cba3f09f2
wojtek <wojtekpietraszuk@gmail.com>
2022-10-07 14:10:31 +0200

    Add links to social media

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,27 @@
 const Footer = () => (
-	<footer className='h-screen flex flex-col items-center justify-center snap-center'>
-		<div>Github</div>
-		<div>Facebook</div>
-		<div>LinkedIn</div>
+	<footer className='h-screen flex flex-col items-center justify-center snap-center relative'>
+		<div className='flex flex-col items-center mb-10'>
+			<a className='text-xl mb-3' href='https://github.com/wojtek0123' rel='noopener'>
+				Github
+			</a>
+			<a
+				className='text-xl my-3'
+				href='https://www.linkedin.com/in/wojciech-pietraszuk-333159199/'
+				rel='noopener'
+			>
+				LinkedIn
+			</a>
+			<a className='text-xl my-3' href='https://www.facebook.com/wojtus.pietraszuk/' rel='noopener'>
+				Facebook
+			</a>
+			<a className='text-xl mt-3 capitalize' href='mailto:wojtekpietraszuk@gmail.com'>
+				send email
+			</a>
+		</div>
+		<p className='absolute bottom-1 left-0 right-0 text-center flex flex-col tracking-wider'>
+			webpage created by Wojciech Pietraszuk
+			<span>{new Date().getFullYear()}</span>
+		</p>
 	</footer>
 );
 
