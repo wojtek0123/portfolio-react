331a4360126aaa4352285a0eaf56e2d2d82c1239
wojtek <wojtekpietraszuk@gmail.com>
2022-10-07 12:35:08 +0200

    Create about me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,11 @@
-import {useEffect, useState} from 'react';
-import backgroundImage from './assets/undraw_startup_life.svg';
 import AboutMe from './components/AboutMe';
-import Projects from './components/Projects';
+import Header from './components/Header';
 
-const slides = [
-	{
-		name: 'about me',
-	},
-	{
-		name: 'home',
-	},
-	{
-		name: 'projects',
-	},
-];
-
-const App = () => {
-	const [slide, setSlide] = useState(1);
-
-	const onNext = () => {
-		setSlide((prevState) => {
-			if (slide >= 2) {
-				return 2;
-			}
-
-			prevState += 1;
-			return prevState;
-		});
-	};
-
-	const onPrev = () => {
-		setSlide((prevState) => {
-			if (slide === 0) {
-				return 0;
-			}
-
-			prevState -= 1;
-			return prevState;
-		});
-	};
-
-	const changeSlide = () => {
-		if (slide === 1) {
-			return '-translate-x-[100vw]';
-		}
-
-		if (slide === 2) {
-			return '-translate-x-[200vw]';
-		}
-
-		return 'translate-x-0';
-	};
-
-	useEffect(() => {
-		console.log(slide);
-	}, [slide]);
-
-	return (
-		<div className='snap-x snap-proximity flex flex-col justify-between bg-black h-screen w-screen text-white overflow-y-hidden overflow-x-scroll'>
-			<div className={`h-full flex w-max transition-transform duration-1000 ${changeSlide()}`}>
-				<AboutMe />
-				<header className='flex flex-col justify-center h-full items-start w-screen snap-center'>
-					<div className='mb-10 px-5'>
-						<span className='text-5xl font-bold'>Hello!</span>
-						<h1 className='text-4xl font-bold mt-5'>I am a Junior Frontend Developer</h1>
-					</div>
-					<div className='flex items-center justify-between text-center w-full px-5 mb-10'>
-						<div className='animate-[slide-left_1s_infinite_alternate]'>slide left</div>
-						<div className='animate-[slide-right_1s_infinite_alternate]'>slide right</div>
-					</div>
-					<img src={backgroundImage} alt='' />
-				</header>
-				<Projects />
-			</div>
-			<nav className='fixed flex justify-between px-5 items-center bg-black bottom-0 left-0 right-0 text-center pb-5'>
-				<button
-					className='text-xl bg-white text-black py-1 px-3 rounded-lg'
-					type='button'
-					onClick={onPrev}
-				>
-					prev
-				</button>
-				<span className='text-xl uppercase'>{slides[slide].name}</span>
-				<button
-					className='text-xl bg-white text-black py-1 px-3 rounded-lg'
-					type='button'
-					onClick={onNext}
-				>
-					next
-				</button>
-			</nav>
-		</div>
-	);
-};
+const App = () => (
+	<div className='snap-y snap-mandatory h-screen overflow-y-scroll bg-black text-white overflow-x-hidden'>
+		<Header />
+		<AboutMe />
+	</div>
+);
 
 export default App;
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,7 +1,20 @@
+import SectionTitle from './SectionTitle';
+import helloBackground from '../assets/undraw_hello.svg';
+
 const AboutMe = () => (
-	<div className='w-screen h-full bg-black snap-center'>
-		<h1>About me</h1>
-	</div>
+	<section className='w-screen h-screen bg-black snap-start flex flex-col items-center justify-center'>
+		<SectionTitle>About Me</SectionTitle>
+		<div className='px-5'>
+			<p className='mb-3 text-xl'>Hi, my name is Wojciech.</p>
+			<p>
+				{`I'm a student of computer science at the Warsaw University of Life
+				Sciences. Currently, I'm in 5th term of engineering studies. My journey with frontend I
+				began in July 2021. Since then, I have been constantly and regurarly gain knowledge about technologies use to built webpages UI.  I'm mostly interesed in new technologies and sports like cycling and
+				running.`}
+			</p>
+		</div>
+		<img src={helloBackground} alt='' className='mt-10' />
+	</section>
 );
 
 export default AboutMe;
