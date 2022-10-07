86c89d1a39268264a08b07543b67f391bf561b53
wojtek <wojtekpietraszuk@gmail.com>
2022-10-07 13:11:17 +0200

    Divide text into more paragraph
    Divide text to make it easier to read

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,12 +5,17 @@ const AboutMe = () => (
 	<section className='w-screen h-screen bg-black snap-start flex flex-col items-center justify-center'>
 		<SectionTitle>About Me</SectionTitle>
 		<div className='px-5'>
-			<p className='mb-3 text-xl'>Hi, my name is Wojciech.</p>
+			<p className='mb-3 text-2xl'>Hi, my name is Wojciech.</p>
 			<p>
-				{`I'm a student of computer science at the Warsaw University of Life
-				Sciences. Currently, I'm in 5th term of engineering studies. My journey with frontend I
-				began in July 2021. Since then, I have been constantly and regurarly gain knowledge about technologies use to built webpages UI.  I'm mostly interesed in new technologies and sports like cycling and
-				running.`}
+				I&apos;m a student of computer science at the Warsaw University of Life Sciences. Currently,
+				I&apos;m in 5th term of engineering studies.
+			</p>
+			<p className='mt-2'>
+				My journey with frontend I began in July 2021. Since then, I have been constantly and
+				regurarly gain knowledge about technologies use to built webpages UI.
+			</p>
+			<p className='mt-2'>
+				I&apos;m mostly interesed in new technologies and sports like cycling and running.
 			</p>
 		</div>
 		<img src={helloBackground} alt='' className='mt-10' />
