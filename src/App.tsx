334b8fe867b2b31b3975ba9a16bb607d68b1c48b
wojtek <wojtekpietraszuk@gmail.com>
2022-10-08 16:47:41 +0200

    Move nav to top from bottom edge of the browser window

--- src/App.tsx
+++ src/App.tsx
@@ -23,23 +23,23 @@ const App = () => {
 					</>
 				)}
 			</div>
-			<div className='fixed bottom-0 left-0 right-0 justify-between items-center pb-5 hidden xl:flex px-10'>
+			<div className='fixed top-0 left-0 right-0 justify-between items-center pt-5 hidden xl:flex px-10'>
 				<h1>Wojciech Pietraszuk</h1>
 				<nav className='flex gap-5'>
 					<a
-						className='relative font-thin text-3xl py-1 px-2 tracking-wider hover:text-yellow-500 transition-colors duration-300 after:content-[""] after:absolute after:-bottom-1 after:left-0 after:w-0 hover:after:w-full after:transition-[width] after:duration-300 after:h-[0.075rem]  after:bg-yellow-500'
+						className='relative font-thin text-3xl py-1 px-2 tracking-wider hover:text-yellow-500 transition-colors duration-300 after:content-[""] after:absolute after:-bottom-1 after:left-0 after:w-0 hover:after:w-full after:transition-[width] after:duration-300 after:h-[0.075rem] after:bg-yellow-500 uppercase'
 						href='#header'
 					>
 						home
 					</a>
 					<a
-						className='relative font-thin text-3xl py-1 px-2 tracking-wider hover:text-yellow-500 transition-colors duration-300 after:content-[""] after:absolute after:-bottom-1 after:left-0 after:w-0 hover:after:w-full after:transition-[width] after:duration-300 after:h-[0.075rem]  after:bg-yellow-500'
+						className='relative font-thin text-3xl py-1 px-2 tracking-wider hover:text-yellow-500 transition-colors duration-300 after:content-[""] after:absolute after:-bottom-1 after:left-0 after:w-0 hover:after:w-full after:transition-[width] after:duration-300 after:h-[0.075rem] after:bg-yellow-500 uppercase'
 						href='#about-me'
 					>
 						about me
 					</a>
 					<a
-						className='relative font-thin text-3xl py-1 px-2 tracking-wider hover:text-yellow-500 transition-colors duration-300 after:content-[""] after:absolute after:-bottom-1 after:left-0 after:w-0 hover:after:w-full after:transition-[width] after:duration-300 after:h-[0.075rem]  after:bg-yellow-500'
+						className='relative font-thin text-3xl py-1 px-2 tracking-wider hover:text-yellow-500 transition-colors duration-300 after:content-[""] after:absolute after:-bottom-1 after:left-0 after:w-0 hover:after:w-full after:transition-[width] after:duration-300 after:h-[0.075rem] after:bg-yellow-500 uppercase'
 						href='#projects'
 					>
 						projects
