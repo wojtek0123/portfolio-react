a84f8958e82d24810446307c14ec4a791fff271c
wojtek <wojtekpietraszuk@gmail.com>
2022-10-09 09:02:14 +0200

    Modify section wrapper
    Change font size for different breakpoint for section title, add styles for section wrapper and use these two comoponents in aboutme section

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
 
 const SectionTitle: React.FC<{children: string}> = ({children}) => (
-	<h3 className='text-center capitalize text-4xl mb-10'>{children}</h3>
+	<h3 className='text-center capitalize text-4xl mb-10 sm:text-5xl xl:text-start xl:text-7xl'>
+		{children}
+	</h3>
 );
 
 SectionTitle.propTypes = {
--- src/components/SectionWrapper.tsx
+++ src/components/SectionWrapper.tsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 
-const SectionWrapper: React.FC<{children: JSX.Element}> = ({children}) => <div>{children}</div>;
+const SectionWrapper: React.FC<{children: JSX.Element}> = ({children}) => (
+	<div className='grid xl:grid-cols-2 max-w-[80vw] w-full'>{children}</div>
+);
 
 SectionWrapper.propTypes = {
 	children: PropTypes.element.isRequired,
