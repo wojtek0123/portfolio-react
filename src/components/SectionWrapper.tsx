40dcbf37f5ce6ffc45a23af69bfb54d8af0bbe14
wojtek <wojtekpietraszuk@gmail.com>
2022-10-10 19:22:54 +0200

    Fix bug with wrong display content
    On high resolution screen with big zoom of the browser the content of the page overlap of each other

--- src/components/SectionWrapper.tsx
+++ src/components/SectionWrapper.tsx
@@ -6,7 +6,7 @@ const SectionWrapper: React.FC<{children: JSX.Element; sectionId: string}> = ({
 }) => (
 	<section
 		id={sectionId}
-		className='relative w-screen h-screen bg-black snap-start flex flex-col items-center justify-center'
+		className='relative w-screen min-h-screen bg-black snap-start flex flex-col items-center justify-center'
 	>
 		<div className='grid xl:grid-cols-2 max-w-[80vw] w-full'>{children}</div>
 	</section>
