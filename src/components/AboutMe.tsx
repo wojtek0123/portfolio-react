f33cb5334e9dbed07d1b06f79a5f5910fefbfcab
wojtek <wojtekpietraszuk@gmail.com>
2022-10-11 07:20:12 +0200

    Create components social links

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,6 +2,7 @@ import SectionTitle from './SectionTitle';
 import helloBackground from '../assets/undraw_hello.svg';
 import ChangeSectionBtn from './ChangeSectionBtn';
 import SectionWrapper from './SectionWrapper';
+import SocialLinks from './SocialLinks';
 
 const AboutMe = () => (
 	<SectionWrapper sectionId='about-me'>
@@ -23,59 +24,7 @@ const AboutMe = () => (
 					</p>
 				</div>
 				<div className='mt-10 hidden sm:px-5 sm:flex sm:flex-col xl:px-0'>
-					<div className='flex gap-5'>
-						<a
-							href='https://www.linkedin.com/in/wojciech-pietraszuk-333159199/'
-							rel='noopener'
-							title='LinkedIn profile'
-							className='flex items-center justify-center'
-						>
-							<svg
-								className='fill-white w-10 h-10 hover:fill-yellow-500 transition-colors duration-300'
-								viewBox='0 0 16 16'
-							>
-								<path d='M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z' />
-							</svg>
-						</a>
-						<a
-							href='https://github.com/wojtek0123'
-							className='flex items-center justify-center'
-							rel='noopener'
-							title='Github profile'
-						>
-							<svg
-								className='fill-white w-10 h-10 hover:fill-yellow-500 transition-colors duration-300'
-								viewBox='0 0 16 16'
-							>
-								<path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z' />
-							</svg>
-						</a>
-						<a
-							href='https://www.facebook.com/wojtus.pietraszuk/'
-							rel='noopener'
-							title='Facebook profile'
-							className='flex items-center justify-center'
-						>
-							<svg
-								className='fill-white hover:fill-yellow-500 transition-colors duration-300 w-10 h-10'
-								viewBox='0 0 16 16'
-							>
-								<path d='M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z' />
-							</svg>
-						</a>
-						<a
-							className='flex items-center justify-center'
-							href='mailto:wojtekpietraszuk@gmail.com'
-							title='send email'
-						>
-							<svg
-								className='fill-white hover:fill-yellow-500 transition-colors duration-300 w-12 h-12'
-								viewBox='0 0 512 512'
-							>
-								<path d='M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z' />
-							</svg>
-						</a>
-					</div>
+					<SocialLinks smallSize={false} />
 					<p className='mt-5 text-2xl tracking-wider'>wojtekpietraszuk@gmail.com</p>
 				</div>
 			</div>
