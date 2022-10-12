60c595c1c25cded2193e1d9fd4835b2fef958c6a
wojtek <wojtekpietraszuk@gmail.com>
2022-10-12 19:07:48 +0200

    Change archor tags to button tags
    Navigation is now using scrollIntoView, instead href in archor tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import AboutMe from './components/AboutMe';
 import Contact from './components/Contact';
 import Header from './components/Header';
@@ -8,6 +8,10 @@ import SocialLinks from './components/SocialLinks';
 
 const App = () => {
 	const [showAllSection, setShowAllSection] = useState(false);
+
+	const navLinksHeader = (sectionId: string) => {
+		document.getElementById(sectionId)?.scrollIntoView();
+	};
 
 	useEffect(() => {
 		setTimeout(() => {
@@ -31,30 +35,42 @@ const App = () => {
 			<div className='fixed top-0 left-0 right-0 justify-between items-center pt-5 hidden xl:flex px-10'>
 				<h1>Wojciech Pietraszuk</h1>
 				<nav className='flex gap-5'>
-					<a
+					<button
 						className='relative font-thin text-3xl py-1 px-2 tracking-wider hover:text-yellow-500 transition-colors duration-300 after:content-[""] after:absolute after:-bottom-1 after:left-0 after:w-0 hover:after:w-full after:transition-[width] after:duration-300 after:h-[0.075rem] after:bg-yellow-500 uppercase'
-						href='#header'
+						type='button'
+						onClick={() => {
+							navLinksHeader('header');
+						}}
 					>
 						home
-					</a>
-					<a
+					</button>
+					<button
 						className='relative font-thin text-3xl py-1 px-2 tracking-wider hover:text-yellow-500 transition-colors duration-300 after:content-[""] after:absolute after:-bottom-1 after:left-0 after:w-0 hover:after:w-full after:transition-[width] after:duration-300 after:h-[0.075rem] after:bg-yellow-500 uppercase'
-						href='#about-me'
+						type='button'
+						onClick={() => {
+							navLinksHeader('about-me');
+						}}
 					>
 						about me
-					</a>
-					<a
+					</button>
+					<button
 						className='relative font-thin text-3xl py-1 px-2 tracking-wider hover:text-yellow-500 transition-colors duration-300 after:content-[""] after:absolute after:-bottom-1 after:left-0 after:w-0 hover:after:w-full after:transition-[width] after:duration-300 after:h-[0.075rem] after:bg-yellow-500 uppercase'
-						href='#projects'
+						type='button'
+						onClick={() => {
+							navLinksHeader('projects');
+						}}
 					>
 						projects
-					</a>
-					<a
+					</button>
+					<button
 						className='relative font-thin text-3xl py-1 px-2 tracking-wider hover:text-yellow-500 transition-colors duration-300 after:content-[""] after:absolute after:-bottom-1 after:left-0 after:w-0 hover:after:w-full after:transition-[width] after:duration-300 after:h-[0.075rem] after:bg-yellow-500 uppercase'
-						href='#contact'
+						type='button'
+						onClick={() => {
+							navLinksHeader('contact');
+						}}
 					>
 						contact
-					</a>
+					</button>
 				</nav>
 				<SocialLinks smallSize={true} />
 			</div>
