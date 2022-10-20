662586a27f151be939835f70141c64b570730feb
wojtek <wojtekpietraszuk@gmail.com>
2022-10-20 10:56:14 +0200

    Install react-draggable and change mobile nav to be draggable
    Mobile navigation is draggable in y axis

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import MyStack from './components/MyStack';
 import Projects from './components/Projects';
 import SocialLinks from './components/SocialLinks';
 import {navLinksHandler} from './utils/utils';
+import Draggable, {DraggableCore} from 'react-draggable';
 
 const App = () => {
 	const [showAllSection, setShowAllSection] = useState(false);
@@ -82,74 +83,82 @@ const App = () => {
 					></span>
 				</button>
 			</nav>
-			<nav
-				className={`fixed bottom-0 left-0 w-full h-max flex flex-col items-center justify-center bg-black py-5 px-5 rounded-t-2xl border-t-2 sm:border-r-2 sm:border-l-2 border-yellow-500 sm:left-1/2 sm:-translate-x-1/2 transition-transform duration-500 ease-in-out sm:max-w-md xl:hidden ${
-					openMenu ? 'translate-y-0' : 'translate-y-full'
-				}`}
+			<Draggable
+				axis='y'
+				grid={[10, 10]}
+				positionOffset={{y: 250, x: 0}}
+				bounds={{left: 0, right: 0, bottom: 0, top: -250}}
 			>
-				<ul className='flex flex-col items-center gap-y-3'>
-					<li>
-						<button
-							type='button'
-							className='text-2xl sm:text-3xl'
-							onClick={() => {
-								navLinksHandler('header');
-								hamburgerHandler();
-							}}
-						>
-							Home
-						</button>
-					</li>
-					<li>
-						<button
-							type='button'
-							className='text-2xl sm:text-3xl'
-							onClick={() => {
-								navLinksHandler('about-me');
-								hamburgerHandler();
-							}}
-						>
-							About me
-						</button>
-					</li>
-					<li>
-						<button
-							type='button'
-							className='text-2xl sm:text-3xl'
-							onClick={() => {
-								navLinksHandler('stack');
-								hamburgerHandler();
-							}}
-						>
-							Stack
-						</button>
-					</li>
-					<li>
-						<button
-							type='button'
-							className='text-2xl sm:text-3xl'
-							onClick={() => {
-								navLinksHandler('projects');
-								hamburgerHandler();
-							}}
-						>
-							Projects
-						</button>
-					</li>
-					<li>
-						<button
-							type='button'
-							className='text-2xl sm:text-3xl'
-							onClick={() => {
-								navLinksHandler('contact');
-								hamburgerHandler();
-							}}
-						>
-							Contact
-						</button>
-					</li>
-				</ul>
-			</nav>
+				<nav
+					className={`fixed bottom-0 left-0 w-full h-max flex flex-col items-center justify-center bg-black py-5 px-5 rounded-t-2xl border-t-2 sm:border-r-2 sm:border-l-2 border-yellow-500 sm:left-1/2 sm:-translate-x-1/2 transition-transform duration-500 ease-in-out sm:max-w-md xl:hidden ${
+						openMenu ? 'translate-y-0' : 'translate-y-full'
+					}`}
+				>
+					<div className='w-16 h-1 bg-white mb-10 rounded'></div>
+					<ul className='flex flex-col items-center gap-y-3'>
+						<li>
+							<button
+								type='button'
+								className='text-2xl sm:text-3xl'
+								onClick={() => {
+									navLinksHandler('header');
+									hamburgerHandler();
+								}}
+							>
+								Home
+							</button>
+						</li>
+						<li>
+							<button
+								type='button'
+								className='text-2xl sm:text-3xl'
+								onClick={() => {
+									navLinksHandler('about-me');
+									hamburgerHandler();
+								}}
+							>
+								About me
+							</button>
+						</li>
+						<li>
+							<button
+								type='button'
+								className='text-2xl sm:text-3xl'
+								onClick={() => {
+									navLinksHandler('stack');
+									hamburgerHandler();
+								}}
+							>
+								Stack
+							</button>
+						</li>
+						<li>
+							<button
+								type='button'
+								className='text-2xl sm:text-3xl'
+								onClick={() => {
+									navLinksHandler('projects');
+									hamburgerHandler();
+								}}
+							>
+								Projects
+							</button>
+						</li>
+						<li>
+							<button
+								type='button'
+								className='text-2xl sm:text-3xl'
+								onClick={() => {
+									navLinksHandler('contact');
+									hamburgerHandler();
+								}}
+							>
+								Contact
+							</button>
+						</li>
+					</ul>
+				</nav>
+			</Draggable>
 			<div className='fixed top-0 left-0 right-0 justify-between items-center pt-5 hidden xl:flex px-10'>
 				<h1>Wojciech Pietraszuk</h1>
 				<nav className='flex gap-5'>
