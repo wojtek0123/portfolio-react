d213995ed3b932cfd91bfc8c2fe127a8bf26dfd6
WojtekP <wojtekpietraszuk@gmail.com>
2023-06-26 11:43:48 +0200

    Change h1 tag to Link
    Change to link to be able to come back to home page

--- src/components/DesktopNavigation.tsx
+++ src/components/DesktopNavigation.tsx
@@ -1,8 +1,11 @@
 import SocialLinks from './SocialLinks';
+import {Link} from 'react-router-dom';
 
 const DesktopNavigation: React.FC = () => (
 	<div className='fixed top-0 left-0 right-0 justify-between items-center pt-5 hidden xl:flex px-10 z-20'>
-		<h1>Wojciech Pietraszuk</h1>
+		<Link to='/' className='text-lg'>
+			Wojciech Pietraszuk
+		</Link>
 		<SocialLinks smallSize={true} />
 	</div>
 );
