fe4960536ff0c340c3764f7091d613f453abd3bf
Wojciech Pietraszuk <87533043+wojtek0123@users.noreply.github.com>
2023-10-29 22:31:52 +0100

    Update date
    Update end date of collaboration with company

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,7 +9,7 @@ const Experience = () => (
 			</div>
 			<div className='flex flex-col'>
 				<span className='text-3xl font-bold text-yellow-500 2xl:text-5xl'>CloudFerro S.A.</span>
-				<span className='mb-3 text-sm 2xl:text-xl'>03.07.2023 - 29.09.2023</span>
+				<span className='mb-3 text-sm 2xl:text-xl'>03.07.2023 - present</span>
 				<span className='mb-5 mt-3 text-xl 2xl:text-2xl'>
 					Trainee Front-end Developer (Angular)
 				</span>
